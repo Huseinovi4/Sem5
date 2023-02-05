82428e2a004861309001ec825b9927ad1131e8be
Aleksey <lexandreeev@gmail.com>
2023-02-05 18:28:48 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿void CrossPoint(double k1, double b1, double k2, double b2)
+{
+  double x = (b1 - b2) / (k2 - k1);
+  double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+  if (k1 == k2) Console.Write("Заданные прямые не пересекаются!");
+  else
+    Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
+}
+
+Console.WriteLine("Input b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Input k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Input b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Input k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+CrossPoint(k1, b1, k2, b2);
