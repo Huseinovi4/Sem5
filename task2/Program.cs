9e9149d4f376b5c7b2c442a7b8d59cbd8847d675
Aleksey <lexandreeev@gmail.com>
2023-02-05 18:36:07 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿double Fibonacci(int n)
+{
+  if (n == 1 || n == 2) return 1;
+  else  return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine()!);
+for(int i =1;i<n+1;i++)
+{
+  Console.WriteLine($"({i})={Fibonacci(i)}");
+}
